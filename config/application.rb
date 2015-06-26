fd140696df6738def6745cb6c87e201b49ef4afc
saidev-t-rajan <saidevrajan@gmail.com>
2015-06-27 00:43:18 +1000

    removed comment line in mongoid.yaml

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,8 @@ module DotaCalculatorMongo
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Mongoid.logger = Logger.new($stdout)
+    # Moped.logger = Logger.new($stdout)
   end
 end

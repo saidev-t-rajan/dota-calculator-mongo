638146751612c5fc39d32bb697060d97d5321900
saidev-t-rajan <saidevrajan@gmail.com>
2015-09-03 23:55:50 +1000

    Added fuzzy search for drop down

---
+++ app/assets/javascripts/fuzzy_dropdown.js
@@ -0,0 +1,62 @@
+$(document).on("page:change", function() {
+  $('#draft_radiant_1').fuzzyDropdown({
+    mainContainer: '#radiant_1_search',
+    arrowUpClass: 'fuzzArrowUp',
+    selectedClass: 'selected',
+    enableBrowserDefaultScroll: true
+  });
+  $('#draft_radiant_2').fuzzyDropdown({
+    mainContainer: '#radiant_2_search',
+    arrowUpClass: 'fuzzArrowUp',
+    selectedClass: 'selected',
+    enableBrowserDefaultScroll: true
+  });
+  $('#draft_radiant_3').fuzzyDropdown({
+    mainContainer: '#radiant_3_search',
+    arrowUpClass: 'fuzzArrowUp',
+    selectedClass: 'selected',
+    enableBrowserDefaultScroll: true
+  });
+  $('#draft_radiant_4').fuzzyDropdown({
+    mainContainer: '#radiant_4_search',
+    arrowUpClass: 'fuzzArrowUp',
+    selectedClass: 'selected',
+    enableBrowserDefaultScroll: true
+  });
+  $('#draft_radiant_5').fuzzyDropdown({
+    mainContainer: '#radiant_5_search',
+    arrowUpClass: 'fuzzArrowUp',
+    selectedClass: 'selected',
+    enableBrowserDefaultScroll: true
+  });
+  $('#draft_dire_1').fuzzyDropdown({
+    mainContainer: '#dire_1_search',
+    arrowUpClass: 'fuzzArrowUp',
+    selectedClass: 'selected',
+    enableBrowserDefaultScroll: true
+  });
+  $('#draft_dire_2').fuzzyDropdown({
+    mainContainer: '#dire_2_search',
+    arrowUpClass: 'fuzzArrowUp',
+    selectedClass: 'selected',
+    enableBrowserDefaultScroll: true
+  });
+  $('#draft_dire_3').fuzzyDropdown({
+    mainContainer: '#dire_3_search',
+    arrowUpClass: 'fuzzArrowUp',
+    selectedClass: 'selected',
+    enableBrowserDefaultScroll: true
+  });
+  $('#draft_dire_4').fuzzyDropdown({
+    mainContainer: '#dire_4_search',
+    arrowUpClass: 'fuzzArrowUp',
+    selectedClass: 'selected',
+    enableBrowserDefaultScroll: true
+  });
+  return $('#draft_dire_5').fuzzyDropdown({
+    mainContainer: '#dire_5_search',
+    arrowUpClass: 'fuzzArrowUp',
+    selectedClass: 'selected',
+    enableBrowserDefaultScroll: true
+  });
+});

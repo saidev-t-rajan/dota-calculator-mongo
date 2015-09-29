2ccaebf9db64482774900f6545ed2bd7b361666c
saidev-t-rajan <saidevrajan@gmail.com>
2015-09-29 23:45:14 +1000

    Fixed up javascript

--- app/assets/javascripts/fuzzy_dropdown.js
+++ app/assets/javascripts/fuzzy_dropdown.js
@@ -1,4 +1,11 @@
 $(document).on("page:change", function() {
+
+  $("form").on("keypress", function (e) {
+    if (e.keyCode == 13) {
+        return false;
+    }
+  });
+
   $('#draft_radiant_1').fuzzyDropdown({
     mainContainer: '#radiant_1_search',
     arrowUpClass: 'fuzzArrowUp',
@@ -53,10 +60,32 @@ $(document).on("page:change", function() {
     selectedClass: 'selected',
     enableBrowserDefaultScroll: true
   });
-  return $('#draft_dire_5').fuzzyDropdown({
+  $('#draft_dire_5').fuzzyDropdown({
     mainContainer: '#dire_5_search',
     arrowUpClass: 'fuzzArrowUp',
     selectedClass: 'selected',
     enableBrowserDefaultScroll: true
   });
+
+  // $('select').on('change', function() {
+  //   if (this.id !== "draft_winrate") {
+  //     var val = this.value;
+  //     var id = this.id;
+  //     var text = this.options[this.selectedIndex].text
+
+  //     $('select').each(function() {
+  //       if (this.id !== id && this.id !== "draft_winrate") {
+  //         $('#'+this.id+' option[value="'+val+'"]').remove();
+  //         if ($('#'+id+'').data('pre_val') !== undefined && $('#'+id+'').data('pre_val') !== '') {
+
+  //           $(this).append($("<option></option>").attr("value", $('#'+id+'').data('pre_val')).text($('#'+id+'').data('pre_text')));
+  //         };
+  //       };
+  //     });
+
+  //     $(this).data('pre_val', val);
+  //     $(this).data('pre_text', text);
+  //   };
+  // });
+
 });
